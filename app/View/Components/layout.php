03159aca22ed07118c75723d20035f8bbb3e6777
Michele <michelecimminorosso52@gmail.com>
2023-02-21 21:11:01 +0100

    Commit 12

--- app/View/Components/layout.php
+++ app/View/Components/layout.php
@@ -21,6 +21,6 @@ class layout extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.layout');
+        return view('layouts.layout');
     }
 }
