cd759b9d2eb133f5c16e9dfd3c3221721bea35ab
filippo <imac@iMac-di-Filippo.local>
2023-02-21 19:42:17 +0100

    Secondo commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-6 mx-auto">
+                <h1>Accedi</h1>
+                <form action="/login" method="POST">
+                    @csrf
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="email" class="form-control">
+                            @error('email') 
+                            <span class="small text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="col-12">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" id="password" class="form-control">
+                            @error('password') 
+                            <span class="small text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div>
+                            <button class="btn btn-primary">Accedi</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</x-layout>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    </head>
+    <body>
+
+        <x-nav />
+        
+        {{ $slot }}
+
+    </body>
+</html>
