18165668777bae46af774958d10c09647fb3f2af
michele <mhl.mion@gmail.com>
2023-02-22 20:30:32 +0100

    inserisci annuncio1

--- database/migrations/2023_02_22_191846_create_annuncements_table.php
+++ database/migrations/2023_02_22_191846_create_annuncements_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('body');
-            $table->decimal('proce', 8, 2);
+            $table->decimal('price', 8, 2);
 
             $table->timestamps();
         });
