e0fda85ddb3a2e0d25c91802dc077de83716d37c
Filippo <gaetafilippo_@hotmail.com>
2023-02-22 20:11:12 +0100

    nav-def

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,22 +1,24 @@
 <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Presto logo</a>
+        <a class="navbar-brand" href="#">Presto.it</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                
-            <li class="nav-item" id="Accedi">
+                @guest
+            <li class="nav-item">
                 <a class="nav-link" href="/login">Accedi</a>
             </li>
-            <li class="nav-item" id="Registrati">
+            <li class="nav-item">
                 <a class="nav-link" href="/register">Registrati</a>
             </li>
-
+            
+            @else
+            
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                
+                {{ Auth::user()->name }}
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
                     <li>
@@ -27,4 +29,7 @@
                     </li>
                 </ul>
             </li>
+            @endguest
+        </div>
+        </div>
 </nav>
