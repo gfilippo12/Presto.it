0b898dd25197b61592d000af0391d8972637463f
Filippo <gaetafilippo_@hotmail.com>
2023-02-22 19:34:13 +0100

    nav-corretta

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,13 +1,19 @@
-<nav>
-    <div id="Presto_logo">
-        <span>Presto logo</span>
-    </div>
-    <div id="Accedi">
-        <span>Accedi</span>
-    </div>
-    <div id="Registrati">
-        <span>Registrati</span>
-    </div>
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Presto logo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                
+            <li class="nav-item" id="Accedi">
+                <a class="nav-link" href="/login">Accedi</a>
+            </li>
+            <li class="nav-item" id="Registrati">
+                <a class="nav-link" href="/register">Registrati</a>
+            </li>
+
     <div id="Raggruppa_1">
         <svg class="Linea_1" viewBox="0 0 34 2">
             <path id="Linea_1" d="M 0 0 L 34 0">
