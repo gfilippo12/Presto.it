00702b38aa55101592c5b08a5b9d8abc61c17ae4
Filippo <gaetafilippo_@hotmail.com>
2023-02-22 20:19:57 +0100

    nav-def1

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Presto.it</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,10 +8,10 @@
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 @guest
             <li class="nav-item">
-                <a class="nav-link" href="/login">Accedi</a>
+                <a class="nav-link" href="/login" id="Accedi">Accedi</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/register">Registrati</a>
+                <a class="nav-link" href="/register" id="Registrati">Registrati</a>
             </li>
             
             @else
