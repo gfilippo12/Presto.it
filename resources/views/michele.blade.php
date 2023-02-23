4f0ac5b1cae23ddcabcd56f2f378158341cc4148
Michele <michelecimminorosso52@gmail.com>
2023-02-23 20:50:22 +0100

    Stile

--- resources/views/michele.blade.php
+++ resources/views/michele.blade.php
@@ -7,19 +7,22 @@
     <i class="bi bi-arrow-up-circle-fill"></i>
     </button>
 
-    <div class="container" id="Gruppo_di_maschere_2">
+    
+    <div class="container">
         <div class="row">
-            <div class="col-12">
-                <h1>Presto, Consegne in tutt'Italia</h1>
-                <button type="text" id="Rettangolo_15">Vendi</button>
-                <button type="text" id="Rettangolo_15">Acquista!</button>
+            <div class="col-12 mx-auto mt-5 ">
+                <h1 class=" text-center d-flex justify-content-center fw-bold mt-5 border border-secondary border-2 rounded" >PRESTO.IT <br> CONSEGNE IN TUTTA ITALIA</h1>
             </div>
-        </div>
-        <div class="row">
-            
+            <div class="custom-shape-divider-bottom-1677180806">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" class="shape-fill"></path>
+                </svg>
+            </div>
         </div>
     </div>
 
+    
+</div>
 
 
         
