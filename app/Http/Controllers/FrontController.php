b16302e620dad92c929aa308ab078ec395d08f86
Filippo <gaetafilippo_@hotmail.com>
2023-02-23 19:42:21 +0100

    categorie1

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Announcement;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
@@ -11,5 +12,10 @@ class FrontController extends Controller
         $announcements = Announcement::take(6)->get()->sortByDesc('created_at');
 
         return view('home', compact('announcements'));
+    }
+
+    public function categoryShow(Category $category) 
+    {
+        return view('categoryShow', compact('category'));
     }
 }
