2f2ce7b35605c7842effa04cec82f880cc00f883
michele <mhl.mion@gmail.com>
2023-02-27 20:27:37 +0100

    revisore button

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Mail\BecomeRevisor;
 use App\Models\Announcement;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Artisan;
 use App\Models\User;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 // * Inizio revisore
 
