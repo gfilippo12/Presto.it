4bdd6c24b559a27767f7170de267afc40e01cacf
michele <mhl.mion@gmail.com>
2023-02-28 19:16:01 +0100

    frontcontroller

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\BecomeRevisor;
 use App\Models\Announcement;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class FrontController extends Controller
 {
@@ -19,6 +24,18 @@ class FrontController extends Controller
         return view('categoryShow', compact('category'));
     }
 
+
+    public function becomeRevisor() 
+    {
+        Mail::to('admin@presto.it')->send(new BecomeRevisor(Auth::user())); //* da vedere
+        return redirect('/')->back()->with('message', 'Complimenti! hai chiesto di diventare revisore in maniera corretta');
+    }
+
+    public function makeRevisor(User $user) 
+    {
+        Artisan::call('presto:makeUserRevisor' , ["email"=>$user->email]);
+        return redirect ('/')->with('message', 'Complimenti! l\'utente è diventato revisore');
+    }
 
 //* VEDERE  BECOME REVISOR  SU VIDEO 3 .... 20:35 REVISOR CONTROLLER (NEL VIDEO 10 LI METTE QUI DENTRO 4:15)!!
 
