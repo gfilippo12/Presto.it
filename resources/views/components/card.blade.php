04d2c8cbdd0de5805a17da3e19d91d710709999d
michele <mhl.mion@gmail.com>
2023-03-01 18:28:17 +0100

    polo

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -14,5 +14,5 @@
                 <p class="card-footer">Pubblicato il: {{ $announcement->created_at->format('d/m/Y') }}</p>
             </div>
         </div>
-    </div>
+    </div>   
 @endforeach
