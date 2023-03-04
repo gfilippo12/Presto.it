3dc50d8684005243b6eb15624558a5dc6e28fd95
michele <mhl.mion@gmail.com>
2023-03-04 13:02:18 +0100

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,22 @@
-<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white d-flex align-items-end">
-    <div class="container text-center">
+<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark d-flex justify-content-center">
+    <div>
         <div class="row">
-            <div class="col-12 text-center">
-                <p>Presto.it</p>
-                <a href="/register"class="btn btn-warning text-light shadow my-3">Registrati!</a>
-                <a href="{{route('become.revisor')}}" class="btn btn-warning text-light shadow my-3">Diventa Revisor</a>   
-            </div>
+        <div class=" col-6  text-white  ">
+          <h4 class=" text-white ">Via degli Sviluppatori web I\0</h4>
+          <p class=" text-white ">Sede legale in via Bruto Caio 9</p>
+          <h5 class=" text-white">PRESTO.IT TEAM</h5>
         </div>
     </div>
+    <div>
+        <div class=" col-6 text-white  ">
+            <h4 class="text-white">Vuoi lavorare con noi?</h4>
+            <p class="text-white">Entra a far parte del Team Presto.it con due semplici click </p>
+            <a href="{{route('become.revisor')}}" class="btn btn-warning text-light shadow my-3">Diventa Revisor</a>
+          </div>
+        </div>
+    </div>
+   
+       
 </footer>
+
+
