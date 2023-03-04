604c226a7f11ccddbd71a35514436626b2f0855b
michele <mhl.mion@gmail.com>
2023-03-04 13:34:40 +0100

    footer2

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,19 +1,22 @@
 <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark d-flex justify-content-center">
-    <div>
+    <div class="container">
         <div class="row">
         <div class=" col-6  text-white  ">
+            <h2 class="text-green">PRESTO.IT</h2>
           <h4 class=" text-white ">Via degli Sviluppatori web I\0</h4>
-          <p class=" text-white ">Sede legale in via Bruto Caio 9</p>
-          <h5 class=" text-white">PRESTO.IT TEAM</h5>
+          <h4 class=" text-white  text-decoration-none"> <a href="06-908070"> Contattaci </a> </h4>
+          <h4 class=" text-white ">Sede Legale: Via dei Bruti 19 </h4>
+          <h4 class=" text-white ">servizio clienti:<a href="link"> Presto.assistenza.clienti@polo.it</a> </h4>
+          
         </div>
-    </div>
-    <div>
         <div class=" col-6 text-white  ">
-            <h4 class="text-white">Vuoi lavorare con noi?</h4>
-            <p class="text-white">Entra a far parte del Team Presto.it con due semplici click </p>
+            
+            <h1 class="text-white">Vuoi lavorare con noi?</h1>
+            <h3 class="text-white">Entra a far parte del Team Presto.it con due semplici click </h3>
             <a href="{{route('become.revisor')}}" class="btn btn-warning text-light shadow my-3">Diventa Revisor</a>
+            
           </div>
-        </div>
+       
     </div>
    
        
