d7be0b39a994cf987cb9ae879769e9a62471410f
Klajdi <saliajklajd@gmail.com>
2023-03-06 21:13:16 +0100

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,25 +1,59 @@
-<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark d-flex justify-content-center">
-    <div class="container">
-        <div class="row">
-        <div class=" col-6  text-white  ">
-            <h2 class="text-green">PRESTO.IT</h2>
-          <h4 class=" text-white ">Via degli Sviluppatori web I\0</h4>
-          <h4 class=" text-white  text-decoration-none"> <a href="06-908070"> Contattaci </a> </h4>
-          <h4 class=" text-white ">Sede Legale: Via dei Bruti 19 </h4>
-          <h4 class=" text-white ">servizio clienti:<a href="link"> Presto.assistenza.clienti@polo.it</a> </h4>
-          
+<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark">
+    
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-white d-flex justify-content-center"><h2>PRESTO.IT</h2></div>
+    </div>
+  </div>
+  <div class="container d-flex justify-content-center">
+    <div class="row">
+      <div class="col-3 text-white "><a href=""><img class="socialicons" src="\social\facebook.png" alt="facebook icon"></a></div>
+      <div class="col-3 text-white "><a href=""><img class="socialicons" src="\social\instagram.png" alt="instagram icon"></a></div>
+      <div class="col-3 text-white "><a href=""><img class="socialicons" src="\social\linkedin.png" alt="linkedin icon"></a></div>
+      <div class="col-3 text-white "><a href=""><img class="socialicons" src="\social\twitter.png" alt="twitter icon"></a></div>
+    </div>
+  </div>
+
+  <hr class="bg-white border-2 border-top border-white">
+  
+  <div class="container">
+        
+    <div class="row">
+        <div class=" col-3  text-white  ">
+          <h2> Contattaci</h2>
+          <div class="d-flex gap-2"><i class="bi bi-geo-alt"></i><p class=" text-white "> Via degli Sviluppatori web I\0, CAP 40030 IT</p></div>
+          <div class="d-flex gap-2"><i class="bi bi-telephone"></i><p class=" text-white"> <a href="06-908070" class="text-decoration-none text-white">(+39)327 4333365 </a></p></div>
+          <div class="d-flex gap-2"><i class="bi bi-envelope"></i></i><p class=" text-white"> info@presto.it</p></div>
         </div>
-        <div class=" col-6 text-white  ">
+        <div class="col-3 text-white">
+          <h2> Categorie</h2>
+          <div><p class=" text-white"> <a href="#" class="text-decoration-none text-white">Tecnologia </a></p></div>
+          <div><p class=" text-white"> <a href="#" class="text-decoration-none text-white">Motori </a></p></div>
+          <div><p class=" text-white"> <a href="#" class="text-decoration-none text-white">Informatica </a></p></div>
+          <div><p class=" text-white"> <a href="#" class="text-decoration-none text-white">Libri </a></p></div>
+        </div> 
+        <div class="col-3 text-white">
+          <h2> Quick Links</h2>
+          <div><p class=" text-white"> <a href="#" class="text-decoration-none text-white">Privacy</a></p></div>
+          <div><p class=" text-white"> <a href="#" class="text-decoration-none text-white">Sicurezza </a></p></div>
+          <div><p class=" text-white"> <a href="#" class="text-decoration-none text-white">Condizioni </a></p></div>
+          <div><p class=" text-white"> <a href="#" class="text-decoration-none text-white">Regole </a></p></div>
+        </div>
+        <div class=" col-3 text-white  ">
             
-            <p class="text-white">Vuoi lavorare con noi?</p>
+            <h2 class="text-white">Vuoi lavorare con noi?</h2>
             <p class="text-white">Entra a far parte del Team Presto.it con due semplici click </p>
             <a href="{{route('become.revisor')}}" class="btn btn-warning text-light shadow my-3">Diventa Revisor</a>
-            
-          </div>
-       
+       </div> 
     </div>
-   
-       
+
+    
+  </div>
+  <hr class="bg-white border-2 border-top border-white">
+
+  <div class="text-white d-flex justify-content-center">
+    <p>© 2023 PRESTO.IT. Tutti i diritti riservati • P.IVA IT07647333333 </p>
+  </div>   
 </footer>
 
 
