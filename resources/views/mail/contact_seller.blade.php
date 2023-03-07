1a75195ad750e38c1019bae5788ea713a0321b75
Michele <michelecimminorosso52@gmail.com>
2023-03-07 20:27:45 +0100

    MAil

--- resources/views/mail/contact_seller.blade.php
+++ resources/views/mail/contact_seller.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="mail.css">
     <title>Presto.it</title>
 </head>
 <body>
@@ -12,7 +13,13 @@
         
         <h2>Ecco i suoi dati</h2>
         <p>Email: {{$user->email}}</p>
+
+        <h2>L'annuncio in questione è {{ $announcement->title }}</h2>
         
+        <h2>Ci auguriamo che la tua Vendita vada a buon fine!</h2>
+        
+        
+        <h2>Un Saluto dallo Staff di Presto.it</h2>
     </div>
     
 </body>
